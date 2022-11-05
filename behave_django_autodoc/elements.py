28f8a25253a5fa87789bd3e333ae023198c14551
Whanderley Souza Freitas <whanderley.souza@iff.edu.br>
2022-11-05 16:41:05 -0300

    Creating scenario class

--- behave_django_autodoc/elements.py
+++ behave_django_autodoc/elements.py
@@ -11,3 +11,19 @@ class Feature(object):
     def __init__(self, feature_dict) -> None:
         self.tittle = feature_dict['tittle']
         self.description = feature_dict.get('description', None)
+
+
+class Scenario(object):
+    '''
+    Scenario object representation
+    fields:
+        tittle: scenario tittle
+        description: scenario description(optional)
+        layout: scenario layout, vertical or horizontal(optional)
+    '''
+    def __init__(self, scenario_dict) -> None:
+        self.tittle = scenario_dict['tittle']
+        self.description = scenario_dict.get('description', None)
+        self.layout = scenario_dict.get('layout', 'vertical')
+        if self.layout not in ['vertical', 'horizontal']:
+            raise ValueError(f'Invalid layout: {self.layout}')
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-from behave_django_autodoc.elements import Feature
+from behave_django_autodoc.elements import Feature, Scenario
 
 
 class TestFeature(unittest.TestCase):
@@ -14,3 +14,26 @@ class TestFeature(unittest.TestCase):
         feature = Feature({'tittle': 'test tittle'})
         self.assertEqual(feature.tittle, 'test tittle')
         self.assertEqual(feature.description, None)
+
+
+class TestScenario(unittest.TestCase):
+
+    def test_init(self):
+        scenario = Scenario({'tittle': 'test tittle', 'description': 'test description', 'layout': 'vertical'})
+        self.assertEqual(scenario.tittle, 'test tittle')
+        self.assertEqual(scenario.description, 'test description')
+        self.assertEqual(scenario.layout, 'vertical')
+
+    def test_init_with_empty_description(self):
+        scenario = Scenario({'tittle': 'test tittle', 'layout': 'vertical'})
+        self.assertEqual(scenario.tittle, 'test tittle')
+        self.assertEqual(scenario.description, None)
+        self.assertEqual(scenario.layout, 'vertical')
+
+    def test_by_default_vertical_layout(self):
+        scenario = Scenario({'tittle': 'test tittle'})
+        self.assertEqual(scenario.layout, 'vertical')
+
+    def test_not_accept_invalid_layout(self):
+        with self.assertRaises(ValueError):
+            Scenario({'tittle': 'test tittle', 'layout': 'invalid'})
