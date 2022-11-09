0a1441f94b7f9d0664a414bc12a40ab5a386e086
Whanderley Souza Freitas <whanderley.souza@iff.edu.br>
2022-11-09 11:36:03 -0300

    Adding output format save methos

--- behave_django_autodoc/output_formats.py
+++ behave_django_autodoc/output_formats.py
@@ -7,15 +7,24 @@ import os.path
 class OutputFormat(object):
     """Class to save the documentation in formats like html, pdf"""
 
-    def __init__(self, html_string, docs_dir):
+    def __init__(self, html_string: str, docs_dir: str, formats: list = None) -> None:
         self.html_string = html_string
         self.docs_dir = docs_dir
+        self.formats = formats
+
+    def save(self) -> None:
+        class_formats = {
+            'html': HtmlFormat
+        }
+        for output_format in self.formats:
+            output_format in class_formats and\
+                class_formats[output_format](self.html_string, self.docs_dir).save()
 
 
 class HtmlFormat(OutputFormat):
     """Class to save the documentation in html format"""
 
-    def save(self):
+    def save(self) -> None:
         html_path = os.path.join(self.docs_dir, "doc.html")
         html_doc = open(html_path, "w+")
         html_doc.writelines(self.html_string)
--- tests/test_output_formats.py
+++ tests/test_output_formats.py
@@ -8,9 +8,16 @@ from behave_django_autodoc.output_formats import OutputFormat
 class TestOutputFormat(unittest.TestCase):
 
     def test_init(self):
-        output_format = OutputFormat(html_string="test html", docs_dir="dir/docs")
+        output_format = OutputFormat(html_string="test html", docs_dir="dir/docs", formats=["html"])
         self.assertEqual(output_format.html_string, "test html")
         self.assertEqual(output_format.docs_dir, "dir/docs")
+        self.assertEqual(output_format.formats, ["html"])
+
+    @mock.patch('behave_django_autodoc.output_formats.HtmlFormat', autospec=True)
+    def test_save(self, mock_html_format):
+        output_format = OutputFormat(html_string="test html", docs_dir="dir/docs", formats=["html"])
+        output_format.save()
+        mock_html_format.assert_called_once_with("test html", "dir/docs")
 
 
 class TestHtmlFormat(unittest.TestCase):
