0048b1fbdcb64be45f5fdda88a7c73baa9fee599
Whanderley Souza Freitas <whanderley.souza@iff.edu.br>
2022-11-15 13:07:27 -0300

    Adding autodoc fuction

--- behave_django_autodoc/__init__.py
+++ behave_django_autodoc/__init__.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa
+from behave_django_autodoc.decorators import *
+
+
 __version__ = "0.1.0"
+
+
+def auto_doc(dict_functions):
+    to_decorate_functions = ["before_all", "after_all", "before_feature",
+                             "after_feature", "before_scenario", "after_scenario",
+                             "before_step", "after_step"]
+    for func in to_decorate_functions:
+        decorator = eval(decorator_for_function(func))
+        if func in dict_functions:
+            dict_functions[func] = decorator(dict_functions[func])
+        else:
+            dict_functions[func] = decorator(lambda *args: None)
+
+
+def decorator_for_function(function_name):
+    words = function_name.split('_')
+    return "".join([w.capitalize() for w in words]) + 'Decorator'
--- tests/test_behave_django_autodoc.py
+++ tests/test_behave_django_autodoc.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
+import unittest
+
 from behave_django_autodoc import __version__
+from behave_django_autodoc import auto_doc
+from behave_django_autodoc import decorator_for_function
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+class TestAutoDoc(unittest.TestCase):
+
+    def test_decorator_for_function(self):
+        function_name = "before_all"
+        self.assertEqual(decorator_for_function(function_name), "BeforeAllDecorator")
+        function_name = "after_all"
+        self.assertEqual(decorator_for_function(function_name), "AfterAllDecorator")
+        function_name = "before_feature"
+        self.assertEqual(decorator_for_function(function_name), "BeforeFeatureDecorator")
+
+    def test_auto_doc(self):
+        dict_functions = {'before_all': lambda *args: None, '__file__': "dir/enviroment.py"}
+        auto_doc(dict_functions)
+        self.assertEqual(dict_functions['before_all'].__class__.__name__, "BeforeAllDecorator")
+        self.assertEqual(dict_functions['after_all'].__class__.__name__, "AfterAllDecorator")
+        self.assertEqual(dict_functions['before_feature'].__class__.__name__, "BeforeFeatureDecorator")
+        self.assertEqual(dict_functions['after_feature'].__class__.__name__, "AfterFeatureDecorator")
+        self.assertEqual(dict_functions['before_scenario'].__class__.__name__, "BeforeScenarioDecorator")
+        self.assertEqual(dict_functions['after_scenario'].__class__.__name__, "AfterScenarioDecorator")
